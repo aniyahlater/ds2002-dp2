9f9f8569c557f556ff1bc0f20c849d0f3c2b6c26
Aniyah McWilliams <ybf3jw@virginia.edu>
2024-04-26 15:28:31 +0000

    draft of the code for project2

---
+++ project2/dbbyme.py
@@ -0,0 +1,28 @@
+from pymongo import MongoClient, errors
+from bson.json_util import dumps
+import os
+import json
+
+MONGOPASS = os.getenv('MONGOPASS')
+uri = "mongodb+srv://cluster0.pnxzwgz.mongodb.net/"
+client = MongoClient(uri, username='nmagee', password=MONGOPASS, connectTimeoutMS=200, retryWrites=True)
+# specify a database
+db = client.project2
+# specify a collection
+collection = db.project2data
+
+#simply opening the data
+#going to to attempt to upload the data now
+directory = "data"
+for filename in os.listdir(directory):
+    with open(os.path.join(directory, filename)) as f:
+        try:
+            file_data = json.load(f)
+            print(f, "successful upload")
+            try:
+                collection.insert_many(file_data)
+                print(file_data, "Succesful Upload")
+                except Exception as e:
+                    print(e, "Error when importing into MONGO")
+
+        
